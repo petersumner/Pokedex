8ce011767f99d294e90ef6e33c503f048aacf3ef
Peter Sumner <petersumner515@gmail.com>
2020-11-08 13:02:32 -0500

    rounded corners

--- Pokedex/Pokedex/View/PokeCell.swift
+++ Pokedex/Pokedex/View/PokeCell.swift
@@ -15,6 +15,12 @@ class PokeCell: UICollectionViewCell {
     
     var pokemon: Pokemon!
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 10.0
+    }
+    
     func configureCell(pokemon: Pokemon) {
         
         self.pokemon = pokemon
