4afbec42b2e6bd839167b3c13a505bec08402244
Peter Sumner <petersumner515@gmail.com>
2020-11-08 11:44:20 -0500

    search bar

---
+++ Pokedex/Pokedex/Model/Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Peter Sumner on 11/8/20.
+//  Copyright © 2020 Peter Sumner. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexID: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexID: Int {
+        return _pokedexID
+    }
+    
+    init(name: String, pokedexID: Int) {
+        self._name = name
+        self._pokedexID = pokedexID
+    }
+}
