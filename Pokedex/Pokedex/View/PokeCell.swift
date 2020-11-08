435a6d5fc21dba0fb50ab9737a7102e6a8e51c22
Peter Sumner <petersumner515@gmail.com>
2020-11-08 12:04:39 -0500

    pokecell

---
+++ Pokedex/Pokedex/View/PokeCell.swift
@@ -0,0 +1,27 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Peter Sumner on 11/8/20.
+//  Copyright © 2020 Peter Sumner. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexID)")
+        
+    }
+    
+}
