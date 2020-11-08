77d1ef1128a32b19f908a0f76d0daf71e8107ff8
Peter Sumner <petersumner515@gmail.com>
2020-11-08 08:26:39 -0500

    fresh start

---
+++ Pokedex/Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Peter Sumner on 11/8/20.
+//  Copyright © 2020 Peter Sumner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
