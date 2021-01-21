11f162b1901fa2a9f5f3c0c88c8134f460435796
Peter Sumner <petersumner515@gmail.com>
2021-01-21 15:23:05 -0500

    detail view

--- Pokedex/Pokedex/Controller/DetailViewController.swift
+++ Pokedex/Pokedex/Controller/DetailViewController.swift
@@ -12,11 +12,30 @@ class DetailViewController: UIViewController {
     
     var pokemon: Pokemon!
     
+    @IBOutlet weak var mainImg: UIImageView!
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var hpLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var spatkLbl: UILabel!
+    @IBOutlet weak var spdefLbl: UILabel!
+    @IBOutlet weak var speedLbl: UILabel!
+    @IBOutlet weak var prevEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         nameLbl.text = pokemon.name
+    }
+    
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
 }
--- Pokedex/Pokedex/Model/Pokemon.swift
+++ Pokedex/Pokedex/Model/Pokemon.swift
@@ -12,6 +12,17 @@ class Pokemon {
     
     private var _name: String!
     private var _pokedexID: Int!
+    private var _description: String!
+    private var _type: String!
+    private var _height: String!
+    private var _weight: String!
+    private var _hp: String!
+    private var _attack: String!
+    private var _defense: String!
+    private var _spatk: String!
+    private var _spdef: String!
+    private var _speed: String!
+    private var _evoTxt: String!
     
     var name: String {
         return _name
