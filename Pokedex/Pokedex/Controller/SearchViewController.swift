adc7dd1f41586645c06b39156ca9766626780bff
Peter Sumner <petersumner515@gmail.com>
2021-02-08 21:30:10 -0500

    search view controller

---
+++ Pokedex/Pokedex/Controller/SearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchViewController.swift
+//  Pokedex
+//
+//  Created by Peter Sumner on 2/8/21.
+//  Copyright © 2021 Peter Sumner. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
