7f630999a4084078537df9421cfede879840bb3f
Peter Sumner <petersumner515@gmail.com>
2021-02-12 13:08:04 -0500

    settings view skeleton

---
+++ Pokedex/Pokedex/Controller/SettingsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SettingsViewController.swift
+//  Pokedex
+//
+//  Created by Peter Sumner on 2/12/21.
+//  Copyright © 2021 Peter Sumner. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
